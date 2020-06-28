17410b0daf6b6c283d5c4011361beab83786e444
Niusha H <niusha.hadziomerovic@gmail.com>
2020-06-28 00:37:38 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
+  get '/user' do
+    @users = User.all
+    erb:'tweets/index'
+  end
+
+  get '/tweet/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'tweets/show'
+  end
 
 end
