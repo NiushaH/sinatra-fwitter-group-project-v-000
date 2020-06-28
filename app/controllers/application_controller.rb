0f4b34985f6bcf56e9a8d8127c1ddac16b8b2af8
Niusha H <niusha.hadziomerovic@gmail.com>
2020-06-28 00:34:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get '/' do
+    erb :index
+  end
+end
 
 end
